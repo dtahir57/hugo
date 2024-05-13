584e6b41a3c91c99f8a33efdf68c068f69b1ff53
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-2.local>
2024-05-13 17:50:47 +0500

    Dashboard updated

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Opportunity;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $opportunities = Opportunity::all()->count();
+        $users = User::doesntHave('roles')->get()->count();
+        return view('admin.dashboard', compact('opportunities', 'users'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,22 +7,30 @@
 @endsection
 
 @section('content')
-<div class="row gy-3 mb-6 justify-content-between">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-header">
-                <h5>Dashboard</h5>
+<div class="row mb-3 gy-6">
+    <div class="col-12 col-xxl-2">
+      <div class="row align-items-center g-3 g-xxl-0 h-100 align-content-between">
+        <div class="col-12 col-sm-6 col-md-3 col-lg-6 col-xl-3 col-xxl-12">
+          <div class="d-flex align-items-center"><span class="fs-4 lh-1 uil uil-books text-primary-dark"></span>
+            <div class="ms-2">
+              <div class="d-flex align-items-end">
+                <h2 class="mb-0 me-2">{{ $opportunities }}</h2><span class="fs-7 fw-semibold text-body">Opportunities</span>
+              </div>
+              <p class="text-body-secondary fs-9 mb-0">Awating processing</p>
             </div>
-            <div class="card-body">
-                <p>{{ __('Admin Dashboard!') }}</p>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-3 col-lg-6 col-xl-3 col-xxl-12">
+          <div class="d-flex align-items-center"><span class="fs-4 lh-1 uil uil-users-alt text-success-dark"></span>
+            <div class="ms-2">
+              <div class="d-flex align-items-end">
+                <h2 class="mb-0 me-2">{{ $users }}</h2><span class="fs-7 fw-semibold text-body">Users</span>
+              </div>
+              <p class="text-body-secondary fs-9 mb-0">Working hard</p>
             </div>
+          </div>
         </div>
-    </div>
-</div>
-<div class="pb-5">
-    <div class="row">
-        <div class="col-12 col-xxl-12">
-        </div>
+      </div>
     </div>
 </div>
 @endsection
