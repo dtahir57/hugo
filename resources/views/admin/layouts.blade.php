a1c1e21ec1f54fbda1a7e1ca66f955ff5a3d3b63
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-2.local>
2024-05-15 04:30:42 +0500

    Updated

--- resources/views/admin/layouts.blade.php
+++ resources/views/admin/layouts.blade.php
@@ -215,11 +215,10 @@
     <!-- ===============================================-->
     <!--    JavaScripts-->
     <!-- ===============================================-->
-    @vite(['resources/assets/vendors/popper/popper.min.js',
-            'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
-            // 'resources/assets/vendors/anchorjs/anchor.min.js',
-            // 'resources/assets/vendors/is/is.min.js',
-            'resources/assets/vendors/fontawesome/all.min.js',
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    
+    @vite(['resources/assets/vendors/fontawesome/all.min.js',
             'resources/assets/vendors/lodash/lodash.min.js',
             // 'https://polyfill.io/v3/polyfill.min.js?features=window.scroll',
             'resources/assets/vendors/list.js/list.min.js',
