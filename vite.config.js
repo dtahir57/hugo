569373fe9934e4fa476fe7bfa283e308a9231111
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-2.local>
2024-05-15 03:29:17 +0500

    Updated

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,6 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/assets/img/favicons/manifest.json',
-                'resources/assets/vendors/simplebar/simplebar.min.js'
             ],
             refresh: true,
         }),
