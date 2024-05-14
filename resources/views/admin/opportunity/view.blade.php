ec023f5883d26198840fe10485f16743710fcca0
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-2.local>
2024-05-15 04:05:56 +0500

    Updated

--- resources/views/admin/opportunity/view.blade.php
+++ resources/views/admin/opportunity/view.blade.php
@@ -155,8 +155,53 @@
     </div>
 </div>
 
-@include('admin.opportunity.delete_modal')
-@include('admin.opportunity.assign_user_modal')
+<form action="{{ route('admin.opportunity.delete', $opportunity->id) }}" method="POST">
+    @csrf
+    <input type="hidden" name="_method" value="DELETE" />
+    <div class="modal fade" id="staticBackdrop" tabindex="-1" data-bs-backdrop="static" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-primary">
+                <h5 class="modal-title text-white dark__text-gray-1100" id="staticBackdropLabel">Delete Opportunity</h5>
+                <button class="btn p-1" type="button" data-bs-dismiss="modal" aria-label="Close"><span class="fas fa-times fs-9 text-white dark__text-gray-1100"></span></button>
+            </div>
+            <div class="modal-body">
+                <p class="text-body-tertiary lh-lg mb-0">Are you sure you want to delete this opportunity?</p>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-primary" type="submit">Delete</button>
+                <button class="btn btn-outline-primary" type="button" data-bs-dismiss="modal">Cancel</button>
+            </div>
+        </div>
+        </div>
+    </div>
+</form>
+<form action="{{ route('admin.opportunity.assign.users', $opportunity->id) }}" method="POST">
+    @csrf
+    <div class="modal modal-lg fade" id="assignUserModal" tabindex="-1" data-bs-backdrop="static" aria-labelledby="assignUserModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-success">
+                <h5 class="modal-title text-white dark__text-gray-1100" id="assignUserModalLabel">Assign User</h5>
+                <button class="btn p-1" type="button" data-bs-dismiss="modal" aria-label="Close"><span class="fas fa-times fs-9 text-white dark__text-gray-1100"></span></button>
+            </div>
+            <div class="modal-body">
+                <label for="organizerMultiple">Select Users (Multiple)</label>
+                <select class="form-select" name="users[]" required id="organizerMultiple" data-choices="data-choices" multiple="multiple" data-options='{"removeItemButton":true,"placeholder":true}'>
+                    <option value="">Select Users</option>
+                    @foreach($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-success" type="submit">Save</button>
+                <button class="btn btn-outline-success" type="button" data-bs-dismiss="modal">Cancel</button>
+            </div>
+        </div>
+        </div>
+    </div>
+</form>
 @endsection
 
 @section('scripts')
