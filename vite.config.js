ab60f5e76a8474c802a08f8ba736f9cabbdeb2c9
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-2.local>
2024-05-15 04:43:53 +0500

    Updated

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
                 'resources/assets/img/favicons/manifest.json',
                 'resources/assets/vendors/simplebar/simplebar.min.js',
                 'resources/assets/js/config.js',
