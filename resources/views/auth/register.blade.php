d5138b88353ca8331db33bb3e37f9dd928206810
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-2.local>
2024-06-26 20:01:23 +0500

    Updated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -64,7 +64,7 @@
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Get Started For 7 Days Free') }}
+                                    {{ __('Get Started') }}
                                 </button>
                             </div>
                         </div>
